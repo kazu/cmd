b92e155bc29dc99b548113bbe9fae6be73d81505
xtakei <xtakei@me.com>
2020-07-26 03:49:20 +0900

    Enable output option
    Add ProgressBar

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/kazu/vfs-index v0.0.0-00010101000000-000000000000
 	github.com/n0madic/twitter-scraper v0.0.0-20200615133851-91f2860d09b3 // indirect
+	github.com/vbauerster/mpb/v5 v5.2.4
 )
 
 //replace github.com/kazu/fbshelper => ../fbshelper/
--- ls2json/main.go
+++ ls2json/main.go
@@ -10,17 +10,22 @@ import (
 	"time"
 
 	vfs "github.com/kazu/vfs-index"
+	"github.com/vbauerster/mpb/v5"
 )
 
 const Usage string = `
-Usage ls2json
+Usage ls2json dir <flags>
+	flags:
+		--output	output json filename
+		--newer		output only newser files mtime.
 `
 
 func main() {
 
-	// if len(os.Args) == 1 {
-	// 	fmt.Println(Usage)
-	// }
+	if len(os.Args) < 2 {
+		fmt.Println(Usage)
+		return
+	}
 	var output string
 	var check string
 	flagcmd := flag.NewFlagSet("ls2json", flag.ExitOnError)
@@ -55,19 +60,41 @@ func main() {
 		log.Println(e)
 		return
 	}
-	fmt.Println(string(result))
+	if output == "" {
+		fmt.Println(string(result))
+		return
+	}
+
+	f, e := os.Create(output)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+	defer f.Close()
+	f.Write(result)
 
 }
 
 type FileRecord struct {
 	Drive   string
 	Inode   uint64
-	Path    string
+	Dir     string
+	Name    string
 	ModTime int64
 	Size    int64
 }
 
 func lsR(dir string, cond time.Time) (result []FileRecord, e error) {
+	//bar := vfs.PBar.Add("finding file"
+	mbar := vfs.NewProgressBar(mpb.WithOutput(os.Stderr))
+
+	total := 65536
+	bar := mbar.Add("search file", total)
+
+	defer func() {
+		bar.SetTotal(bar.Current(), true)
+		mbar.Done()
+	}()
 
 	e = filepath.Walk(dir,
 		func(path string, info os.FileInfo, err error) error {
@@ -84,10 +111,17 @@ func lsR(dir string, cond time.Time) (result []FileRecord, e error) {
 			}
 			//fmt.Println(path, info.Size())
 			rel, _ := filepath.Rel(dir, path)
+			bar.IncrBy(1)
+			if bar.Current() >= (int64(total) - 10) {
+				total += (total / 2)
+				bar.SetTotal(int64(total), false)
+
+			}
 			result = append(result, FileRecord{
 				Drive:   dir,
 				Inode:   vfs.GetInode(info),
-				Path:    rel,
+				Dir:     filepath.Dir(rel),
+				Name:    filepath.Base(rel),
 				ModTime: info.ModTime().UnixNano(),
 				Size:    info.Size(),
 			})
